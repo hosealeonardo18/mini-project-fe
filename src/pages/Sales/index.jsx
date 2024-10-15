607ba456a72a258dc29e8148a95c3aae13cc23cd
hosealeonardo18 <hosealeonardo18@gmail.com>
2024-10-15 23:47:13 +0700

    on progress modul penjualan

--- src/pages/Sales/index.jsx
+++ src/pages/Sales/index.jsx
@@ -9,6 +9,35 @@ const Sales = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [salesItems, setSalesItems] = useState([
+    { barang: "", qty: 0, harga: 0 },
+  ]);
+
+  const handleInputChange = (index, event) => {
+    const { name, value } = event.target;
+    const newItems = [...salesItems];
+
+    if (name == "qty") {
+      const calculation = value * 10000;
+      newItems[index]["harga"] = calculation; // Update the specific field in the row
+    }
+
+    newItems[index][name] = value; // Update the specific field in the row
+    setSalesItems(newItems); // Update the state
+  };
+
+  // Function to add a new row
+  const addRow = () => {
+    setSalesItems([...salesItems, { barang: "", qty: 0, harga: 0 }]);
+  };
+
+  // Function to remove a row by index
+  const removeRow = (index) => {
+    const newItems = salesItems.filter((_, i) => i !== index);
+    setSalesItems(newItems);
+  };
+
+  const handleChange = () => {};
   return (
     <Content>
       <nav className="mb-4" aria-label="breadcrumb">
@@ -20,11 +49,85 @@ const Sales = () => {
         </ol>
       </nav>
 
-      <div className="mb-3">
-        <Button variant="primary" onClick={handleShow}>
-          <FontAwesomeIcon icon={faPlus} style={{ marginRight: "10px" }} />
-          Penjualan
-        </Button>
+      <div className="container p-0 m-0" style={{ fontSize: "0.9rem" }}>
+        <div className="row">
+          <div className="col-lg-8">
+            <div className="card p-4">
+              <div className="mb-3">
+                <div className="form-group">
+                  <label for="tambah_pelanggan">Pelanggan</label>
+                  <input
+                    type="text"
+                    name="kode_pelanggan"
+                    className="form-control"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+
+              <div className="container m-0 p-0">
+                <form>
+                  {salesItems.map((item, index) => (
+                    <div className="row mb-3" key={index}>
+                      <div className="col-4">
+                        <label>Barang</label>
+                        <input
+                          type="text"
+                          name="barang"
+                          value={item.barang}
+                          className="form-control"
+                          onChange={(e) => handleInputChange(index, e)}
+                        />
+                      </div>
+                      <div className="col-3">
+                        <label>Qty</label>
+                        <input
+                          type="number"
+                          name="qty"
+                          value={item.qty}
+                          className="form-control"
+                          onChange={(e) => handleInputChange(index, e)}
+                        />
+                      </div>
+                      <div className="col-3">
+                        <label>Harga</label>
+                        <input
+                          type="text"
+                          name="harga"
+                          value={item.harga}
+                          className="form-control"
+                          onChange={(e) => handleInputChange(index, e)}
+                          disabled
+                        />
+                      </div>
+                      <div className="col-2 d-flex align-items-end">
+                        <button
+                          type="button"
+                          className="btn btn-danger"
+                          onClick={() => removeRow(index)}
+                        >
+                          Hapus
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={addRow}
+                  >
+                    Tambah Field
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4">
+            <div className="card p-4"></div>
+          </div>
+        </div>
       </div>
     </Content>
   );
