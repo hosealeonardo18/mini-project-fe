4839616fd3a7479d6b82fa3cff9c155088efe277
hosealeonardo18 <hosealeonardo18@gmail.com>
2024-10-24 21:54:48 +0700

    create penjualan done

---
+++ src/pages/Sales/DataTable/index.jsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
+
+const DataTables = ({ columns, data, progress, title }) => {
+  const [pending, setPending] = useState(true);
+  const [search, setSearch] = useState("");
+  const [filteredData, setFilteredData] = useState(data);
+
+  useEffect(() => {
+    if (!progress) {
+      setPending(false);
+    }
+
+    // Ensure that the data is properly filtered when `data` is updated
+    const filteredItems = data?.filter(
+      (item) =>
+        item.nama.toLowerCase().includes(search.toLowerCase()) ||
+        item.domisili.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setFilteredData(filteredItems);
+  }, [data, progress, search]);
+
+  const customStyles = {
+    header: {
+      style: {
+        color: "black", // white text
+        borderRadius: "0px", // Remove border-radius
+      },
+    },
+    rows: {
+      style: {
+        minHeight: "30px", // optional, adjust row height
+        border: "1px solid #ddd", // Adding border for all sides
+        borderRadius: "0px", // Remove border-radius
+      },
+    },
+    cells: {
+      style: {
+        padding: "8px", // Adjust padding
+        color: "#073642", // default text color
+        borderLeft: "1px solid #ddd", // Left border for cells
+        borderRight: "1px solid #ddd", // Right border for cells
+        borderRadius: "0px", // Remove border-radius
+      },
+    },
+    headCells: {
+      style: {
+        textTransform: "uppercase",
+        color: "black", // white text
+        borderRadius: "0px", // Remove border-radius
+        border: "1px solid #ddd", // Border for header cells
+      },
+    },
+  };
+
+  const conditionalRowStyles = [
+    {
+      when: (row, index) => index % 2 === 0, // even rows
+      style: {
+        backgroundColor: "#FFA500", // orange for even rows
+        "&:hover": {
+          backgroundColor: "#FFB347", // lighter orange on hover for even rows
+        },
+      },
+    },
+    {
+      when: (row, index) => index % 2 !== 0, // odd rows
+      style: {
+        backgroundColor: "#FFFFFF", // white for odd rows
+        "&:hover": {
+          backgroundColor: "#F0F0F0", // light gray on hover for odd rows
+        },
+      },
+    },
+  ];
+
+  return (
+    <div className="card p-4">
+      <input
+        type="text"
+        placeholder={`Cari ${title}`}
+        className="form-control mb-3 col-lg-3 col-sm-12"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <DataTable
+        columns={columns}
+        data={filteredData}
+        customStyles={customStyles}
+        conditionalRowStyles={conditionalRowStyles}
+        striped
+        highlightOnHover
+        pagination
+        progressPending={pending}
+      />
+    </div>
+  );
+};
+
+export default DataTables;
--- src/pages/Sales/index.jsx
+++ src/pages/Sales/index.jsx
@@ -1,29 +1,89 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Content from "../../components/Content";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "react-bootstrap";
+import Select from "react-select";
+import axios from "axios";
+import Swal from "sweetalert2";
+import DataTables from "./DataTable";
 
 const Sales = () => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  // select Option Customer
+  const [selectedOptionCustomer, setSelectedOptionCustomer] = useState(null);
+  const [optionCustomers, setOptionCustomers] = useState([]);
+
+  // sales item
   const [salesItems, setSalesItems] = useState([
-    { barang: "", qty: 0, harga: 0 },
+    { barang: "", name: "", qty: 0, harga: 0 },
   ]);
 
-  const handleInputChange = (index, event) => {
-    const { name, value } = event.target;
+  // select Option Customer
+  const [optionProducts, setOptionProducts] = useState([]);
+
+  // get data Customer
+  const urlCustomer = `${process.env.REACT_APP_URL}/api/pelanggan?limit=1000`;
+
+  // get data product
+  const urlProduct = `${process.env.REACT_APP_URL}/api/barang?limit=1000`;
+
+  // post data transaction
+  const urlTransaction = `${process.env.REACT_APP_URL}/api/penjualan`;
+
+  // function get data for select option
+  const getDataFromApi = (url, state) => {
+    axios
+      .get(url)
+      .then((response) => {
+        const resData = response.data;
+
+        if (!resData.error) {
+          const formattedOptions = resData.data.map((item) => ({
+            value: item.uid || item.kode,
+            label: item.nama,
+            price: item.harga || "",
+          }));
+
+          state(formattedOptions);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    // call customers
+    getDataFromApi(urlCustomer, setOptionCustomers);
+
+    // call products
+    getDataFromApi(urlProduct, setOptionProducts);
+  }, [urlCustomer, urlProduct]);
+
+  const handleInputChange = (index, event, selectType = false) => {
     const newItems = [...salesItems];
 
-    if (name == "qty") {
-      const calculation = value * 10000;
-      newItems[index]["harga"] = calculation; // Update the specific field in the row
+    if (selectType === "barang") {
+      newItems[index]["barang"] = event.value;
+      newItems[index]["price"] = event.price;
+      newItems[index]["name"] = event.label;
+    } else {
+      const { name, value } = event.target;
+
+      if (name === "qty") {
+        const price = newItems[index]["price"] || 0;
+        const calculation = value * price;
+        newItems[index]["harga"] = calculation;
+      }
+
+      newItems[index][name] = value;
     }
 
-    newItems[index][name] = value; // Update the specific field in the row
-    setSalesItems(newItems); // Update the state
+    setSalesItems(newItems); // Update state dengan data terbaru
   };
 
   // Function to add a new row
@@ -37,7 +97,59 @@ const Sales = () => {
     setSalesItems(newItems);
   };
 
-  const handleChange = () => {};
+  const handleChange = (option) => {
+    setSelectedOptionCustomer(option);
+  };
+
+  const formatCurrency = (value, currency = "IDR") => {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: currency,
+      minimumFractionDigits: 0, // Tidak ada desimal
+    }).format(value);
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("kode_pelanggan", selectedOptionCustomer.value);
+
+    salesItems.forEach((item, index) => {
+      formData.append(`barang[${index}][kode]`, item.barang);
+      formData.append(`barang[${index}][qty]`, item.qty);
+    });
+
+    axios
+      .post(urlTransaction, formData)
+      .then((res) => {
+        if (!res.data.false) {
+          Swal.fire({
+            title: "Successfully!",
+            text: `${res.data.message}`,
+            icon: "success",
+            timer: 2000,
+            showConfirmButton: true,
+            willClose: () => {
+              setSalesItems([{ barang: "", name: "", qty: 0, harga: 0 }]);
+              setOptionProducts([]);
+              setOptionCustomers([]);
+            },
+          });
+        } else {
+          Swal.fire({
+            title: "Failed!",
+            text: res.data.message,
+            icon: "failed",
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <Content>
       <nav className="mb-4" aria-label="breadcrumb">
@@ -49,37 +161,50 @@ const Sales = () => {
         </ol>
       </nav>
 
-      <div className="container p-0 m-0" style={{ fontSize: "0.9rem" }}>
-        <div className="row">
-          <div className="col-lg-8">
-            <div className="card p-4">
-              <div className="mb-3">
-                <div className="form-group">
-                  <label for="tambah_pelanggan">Pelanggan</label>
-                  <input
-                    type="text"
-                    name="kode_pelanggan"
-                    className="form-control"
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
+      <form onSubmit={handleSubmitForm}>
+        <div className="mb-3">
+          <button type="submit" className="btn btn-success">
+            Create
+          </button>
+        </div>
 
-              <div className="container m-0 p-0">
-                <form>
+        <div className="container p-0 m-0" style={{ fontSize: "0.9rem" }}>
+          <div className="row">
+            <div className="col-lg-8 mb-3">
+              <div className="card p-4">
+                <div className="mb-3">
+                  <div className="form-group">
+                    <label htmlFor="tambah_pelanggan">Pelanggan</label>
+                    <Select
+                      value={selectedOptionCustomer}
+                      onChange={handleChange}
+                      options={optionCustomers}
+                      isSearchable={true}
+                      placeholder="Pilih Pelanggan..."
+                    />
+                  </div>
+                </div>
+
+                <div className="container m-0 p-0">
                   {salesItems.map((item, index) => (
-                    <div className="row mb-3" key={index}>
-                      <div className="col-4">
+                    <div className="row" key={index}>
+                      <div className="col-lg-4 mb-3">
                         <label>Barang</label>
-                        <input
-                          type="text"
+                        <Select
                           name="barang"
-                          value={item.barang}
-                          className="form-control"
-                          onChange={(e) => handleInputChange(index, e)}
+                          value={optionProducts.find(
+                            (opt) => opt.value === item.barang
+                          )}
+                          onChange={(selectedOption) =>
+                            handleInputChange(index, selectedOption, "barang")
+                          }
+                          options={optionProducts}
+                          isSearchable={true}
+                          placeholder="Pilih Barang..."
                         />
                       </div>
-                      <div className="col-3">
+
+                      <div className="col-lg-3 mb-3">
                         <label>Qty</label>
                         <input
                           type="number"
@@ -89,7 +214,7 @@ const Sales = () => {
                           onChange={(e) => handleInputChange(index, e)}
                         />
                       </div>
-                      <div className="col-3">
+                      <div className="col-lg-3 mb-3">
                         <label>Harga</label>
                         <input
                           type="text"
@@ -100,7 +225,7 @@ const Sales = () => {
                           disabled
                         />
                       </div>
-                      <div className="col-2 d-flex align-items-end">
+                      <div className="col-lg-2 mb-3 d-flex align-items-end">
                         <button
                           type="button"
                           className="btn btn-danger"
@@ -119,15 +244,52 @@ const Sales = () => {
                   >
                     Tambah Field
                   </button>
-                </form>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4">
+              <div className="card p-4">
+                <h6 style={{ fontWeight: 700, color: "black" }}>Detail Item</h6>
+                <hr />
+                {salesItems && salesItems.length > 0
+                  ? salesItems.map((item, index) => {
+                      if (item.barang && item.qty && item.harga) {
+                        return (
+                          <div className="item-detail mb-3" key={index}>
+                            <div className="d-flex justify-content-between">
+                              <span>{item.name}</span>
+                              <span>{item.qty}x</span>
+                              <span>Rp. {item.harga}</span>
+                            </div>
+                          </div>
+                        );
+                      } else {
+                        return null;
+                      }
+                    })
+                  : "Please add item"}
+
+                <hr />
+                <div className="d-flex justify-content-between total-harga">
+                  <h6 style={{ fontWeight: 700, color: "black" }}>Sub Total</h6>
+                  <h5 style={{ fontWeight: 700, color: "black" }}>
+                    {formatCurrency(
+                      salesItems
+                        .filter((item) => item.barang && item.qty && item.harga)
+                        .reduce((acc, item) => acc + item.harga, 0)
+                    )}
+                  </h5>
+                </div>
               </div>
             </div>
           </div>
-
-          <div className="col-lg-4">
-            <div className="card p-4"></div>
-          </div>
         </div>
+      </form>
+
+      <div className="mb-5 mt-3">
+        <h5 style={{ fontWeight: 700, color: "black" }}>List Penjualan</h5>
+        <DataTables />
       </div>
     </Content>
   );
